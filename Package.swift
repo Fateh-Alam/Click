f99e5c910e113b84ec543bac713745ce253a7f6e
Fateh Alam <mnbhatty.fab@gmail.com>
2021-06-10 14:57:42 +0500

    Added BlueButtonStyle

--- Package.swift
+++ Package.swift
@@ -21,8 +21,8 @@ let package = Package(
         .target(
             name: "Click",
             dependencies: []),
-        .testTarget(
-            name: "ClickTests",
-            dependencies: ["Click"]),
+//        .testTarget(
+//            name: "ClickTests",
+//            dependencies: ["Click"]),
     ]
 )
--- Sources/Click/Click.swift
+++ Sources/Click/Click.swift
@@ -1,3 +1,16 @@
-struct Click {
-    var text = "Hello, World!"
+
+
+import SwiftUI
+
+@available(iOS 13.0, *)
+@available(macOS 10.15, *)
+struct BlueButtonStyle: ButtonStyle{
+    func makeBody(configuration: Configuration) -> some View {
+        configuration.label
+            .font(.title)
+            .foregroundColor(Color.white)
+            .padding()
+            .background(Color.blue)
+            .cornerRadius(15)
+    }
 }
