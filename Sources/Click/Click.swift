bd2102fe614ee1ed3e25b891e3f562bcbec2bc24
Fateh Alam <mnbhatty.fab@gmail.com>
2021-06-10 22:14:20 +0500

    Update Click.swift

--- Sources/Click/Click.swift
+++ Sources/Click/Click.swift
@@ -4,8 +4,8 @@ import SwiftUI
 
 @available(iOS 13.0, *)
 @available(macOS 10.15, *)
-struct BlueButtonStyle: ButtonStyle{
-    func makeBody(configuration: Configuration) -> some View {
+public struct BlueButtonStyle: ButtonStyle{
+    public func makeBody(configuration: Configuration) -> some View {
         configuration.label
             .font(.title)
             .foregroundColor(Color.white)
