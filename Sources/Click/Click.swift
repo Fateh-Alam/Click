8e4d47028613e4452f404d3ea0b74ac60d5d42b4
Fateh Alam <mnbhatty.fab@gmail.com>
2021-06-11 09:58:02 +0500

    Update Click.swift

--- Sources/Click/Click.swift
+++ Sources/Click/Click.swift
@@ -10,11 +10,7 @@ public struct BlueButtonStyle: ButtonStyle{
     public var color: Color = Color.blue
     public var textColor: Color = Color.white
     
-    public init(cornerRadii: CGFloat?, background: Color?, foregroundColor: Color?){
-        self.cornerRadius = cornerRadii ?? 16
-        self.color = background ?? Color.blue
-        self.textColor = foregroundColor ?? Color.white
-    }
+    public init(){}
     
     public func makeBody(configuration: Configuration) -> some View {
         configuration.label
@@ -26,28 +22,66 @@ public struct BlueButtonStyle: ButtonStyle{
     }
 }
 
-//@available(iOS 13.0, *)
-//@available(macOS 10.15, *)
-//public struct CustomBlueButtonStyle: ButtonStyle{
-//
-//    public var cornerRadius: CGFloat = 16
-//    public var color: Color = Color.blue
-//    public var textColor: Color = Color.white
-//
-//    public init(cornerRadii: CGFloat, background: Color, foregroundColor: Color){
-//        self.cornerRadius = cornerRadii
-//        self.color = background
-//        self.textColor = foregroundColor
-//    }
-//    public func makeBody(configuration: Configuration) -> some View {
-//        configuration.label
-//            .font(.title)
-//            .foregroundColor(textColor)
-//            .padding()
-//            .background(color)
-//            .cornerRadius(cornerRadius)
-//    }
-//}
+@available(iOS 14.0, *)
+@available(macOS 10.15, *)
+public struct CustomButtonStyle: ButtonStyle{
+
+    public var cornerRadius: CGFloat
+    public var color: Color
+    public var textColor: Color
+    public var isNeumorphic: Bool
+
+    public init(cornerRadii: CGFloat, background: Color, foregroundColor: Color, neumorphic: Bool){
+        self.cornerRadius = cornerRadii
+        self.color = background
+        self.textColor = foregroundColor
+        self.isNeumorphic = neumorphic
+    }
+    public func makeBody(configuration: Configuration) -> some View {
+        configuration.label
+            .font(.title)
+            .foregroundColor(textColor)
+            .padding()
+            .background(self.isNeumorphic ? color : Color.clear)
+            .cornerRadius(cornerRadius)
+            .background(
+                self.isNeumorphic ?
+                    ZStack {
+                        Color.clear
+                    
+                    RoundedRectangle(cornerRadius: cornerRadius, style: .continuous)
+                        .foregroundColor(.clear)
+                        .blur(radius: 4)
+                        .offset(x: -8, y: -8)
+                    
+                    RoundedRectangle(cornerRadius: cornerRadius, style: .continuous)
+                        .fill(
+                            LinearGradient(gradient: Gradient(colors: [Color(UIColor(Color.clear)), Color.clear]), startPoint: .topLeading, endPoint: .bottomTrailing)
+                        )
+                        .padding(2)
+                        .blur(radius: 2)
+                }
+                    :
+                ZStack {
+                    Color(#colorLiteral(red: 0.7608050108, green: 0.8164883852, blue: 0.9259157777, alpha: 1))
+                    
+                    RoundedRectangle(cornerRadius: cornerRadius, style: .continuous)
+                        .foregroundColor(.white)
+                        .blur(radius: 4)
+                        .offset(x: -8, y: -8)
+                    
+                    RoundedRectangle(cornerRadius: cornerRadius, style: .continuous)
+                        .fill(
+                            LinearGradient(gradient: Gradient(colors: [Color(#colorLiteral(red: 0.9019607843, green: 0.9294117647, blue: 0.9882352941, alpha: 1)), Color.white]), startPoint: .topLeading, endPoint: .bottomTrailing)
+                        )
+                        .padding(2)
+                        .blur(radius: 2)
+                }
+                
+            )
+        
+    }
+}
 
 //@available (iOS 14.0, *)
 //@available (macOS 10.0, *)
